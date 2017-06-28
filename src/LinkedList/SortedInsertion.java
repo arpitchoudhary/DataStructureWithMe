f2168dfb3c425c64817fbc50bd6a72d359d5fa25
Choudhary, Arpit <AChoudhary@mast.com>
2017-06-28 23:19:32 +0530

    adding driver implementation for sortedInsertion

--- src/LinkedList/SortedInsertion.java
+++ src/LinkedList/SortedInsertion.java
@@ -9,9 +9,38 @@ public class SortedInsertion extends LinkedList {
     public static void main(String... args) {
 
         SortedInsertion insertion = new SortedInsertion();
-        insertion.createSampleLinkedList(0, 1);
 
-        insertion.addNodeInSortedOrder(new Node(4));
+        insertion.createSingleNode(4);
+
+        insertion.traverseLinkedList("created List, 4 data");
+
+        // insert node at first position
+        insertion.addNodeInSortedOrder(new Node(2));
+
+        insertion.traverseLinkedList("adding 2");
+
+        insertion.addNodeInSortedOrder(new Node(3));
+
+        insertion.traverseLinkedList("added 3");
+
+        insertion.addNodeInSortedOrder(new Node(7));
+
+        insertion.traverseLinkedList("added 7");
+
+        insertion.head = null; // could be more better know
+
+        insertion.traverseLinkedList("clear the list");
+
+        insertion.addNodeInSortedOrder(new Node(10));
+
+        insertion.traverseLinkedList("added first element");
+
+
+
+
+
+
+
 
     }
 }
